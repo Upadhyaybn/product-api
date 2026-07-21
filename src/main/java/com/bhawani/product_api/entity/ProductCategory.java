cc6f393b00506f1bfff1583941a79adad2774ef1
Bhawani Nandan Upadhyay <upadhyaybn321@gmail.com>
2026-07-21 09:52:45 +0530

    ProductCategory update

--- src/main/java/com/bhawani/product_api/entity/ProductCategory.java
+++ src/main/java/com/bhawani/product_api/entity/ProductCategory.java
@@ -1,11 +1,14 @@
 package com.bhawani.product_api.entity;
 
+import jakarta.persistence.Entity;
+
 /**
  * Defines the supported product categories.
  *
  * Enum is used to restrict product categories
  * to a fixed set of valid values.
  */
+
 public enum ProductCategory {
 
     ELECTRONICS,
